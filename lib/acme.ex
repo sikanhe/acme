de1fdd703772eaaba0ea7bbdefbc99eac01011dd
sikanhe <sikanh@gmail.com>
2017-03-18 15:59:57 -0400

    first commit

---
+++ lib/acme.ex
@@ -0,0 +1,18 @@
+defmodule Acme do
+  @moduledoc """
+  Documentation for Acme.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Acme.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/acme_test.exs
@@ -0,0 +1,8 @@
+defmodule AcmeTest do
+  use ExUnit.Case
+  doctest Acme
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
