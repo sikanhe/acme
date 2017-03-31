79b94ed3fcd0f6dea1f7e1721fc64462fc76896a
sikanhe <sikanh@gmail.com>
2017-03-31 00:19:56 -0400

    0.2.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Acme.Mixfile do
 
   def project do
     [app: :acme,
-     version: "0.1.0",
+     version: "0.2.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -17,6 +17,10 @@ defmodule Acme.Mixfile do
   defp deps do
     [{:hackney, "~> 1.7.1"},
      {:poison, "~> 3.1.0"},
-     {:jose, "~> 1.8.2"}]
+     {:jose, "~> 1.8.2"},
+     # Docs
+     {:ex_doc, "~> 0.10", only: :dev},
+     {:earmark, "~> 0.1", only: :dev},
+     {:inch_ex, ">= 0.0.0", only: :dev}]
   end
 end
