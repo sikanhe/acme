b0726234b3da85b63f334f276fa088c6dc31c04c
Sikan He <sikanh@gmail.com>
2017-07-18 23:20:31 -0400

    0.4.0

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Acme.Mixfile do
 
   def project do
     [app: :acme,
-     version: "0.3.0",
+     version: "0.4.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
